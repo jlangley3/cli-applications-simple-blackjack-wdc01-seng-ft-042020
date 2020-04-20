fddd5a5760b87f9622a9cc7f803387243548fd91
Jesse Langley <jlangleyiii@aol.com>
2020-04-20 02:36:55 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -7,7 +7,15 @@ def deal_card
 end
 
 def display_card_total
-  # code #display_card_total here
+  describe "#display_card_total" do
+  it "accepts one argument, the card total" do
+    expect { display_card_total(7) }.to_not raise_error
+  end
+ 
+  it "prints the value of the cards to the screen" do
+    expect($stdout).to receive(:puts).with("Your cards add up to 8")
+    display_card_total(8)
+  end# code #display_card_total here
 end
 
 def prompt_user
